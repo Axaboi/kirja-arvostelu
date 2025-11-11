54309c78407bf0c438d5beed83eb803ca2bd33bc
Axaboi <axelyancik9@gmail.com>
2025-11-11 18:55:01 +0200

    Add form for new books, update schema.sql, app.py and index.html

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_book")
+def new_book():
+    return render_template("new_book.html")
+
+@app.route("/create_book", methods="[POST]")
+def create_book():
+    title = request.form["title"]
+    description = request.form["description"]
+    book_grade = request.form["book_grade"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO books (title, description, book_grade, user_id)
+             VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, description, book_grade, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,8 +59,10 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
     
     if check_password_hash(password_hash, password):
         session["username"] = username
@@ -53,5 +72,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT unique,
     password_hash TEXT
 );
+
+CREATE TABLE books(
+    if INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    book_grade INTEGER,
+    user_id INTEGER REFERENCES users
+);
