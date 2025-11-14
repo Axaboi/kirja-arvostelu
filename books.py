86d095e731deb016b0603ee66ddaf90d5c5eb965
Axaboi <axelyancik9@gmail.com>
2025-11-14 11:22:22 +0200

    Add show_book.html to templates, change app.py, books.py and index.html

--- books.py
+++ books.py
@@ -4,3 +4,18 @@ def add_book(title, description, book_grade, user_id):
     sql = """INSERT INTO books (title, description, book_grade, user_id)
              VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, book_grade, user_id])
+
+
+def get_books():
+    sql = """SELECT id, title FROM books ORDER BY id DESC"""
+    return db.query(sql)
+
+def get_book(book_id):
+    sql = """SELECT books.title,
+                    books.description,
+                    books.book_grade,
+                    users.username
+             FROM books, users
+             WHERE books.user_id = users.id AND
+                   books.id = ?"""
+    return db.query(sql, [book_id])[0]
