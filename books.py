803ca11c9c32dbe508d6a02f38ac93aaca8af312
Axaboi <axelyancik9@gmail.com>
2025-12-12 13:45:42 +0200

    Fix removal for when book has comments

--- books.py
+++ books.py
@@ -66,6 +66,8 @@ def update_book(book_id, title, description, book_grade, author, classes):
         db.execute(sql, [book_id, title, value])
 
 def remove_book(book_id):
+    sql = "DELETE FROM comments WHERE book_id = ?"
+    db.execute(sql, [book_id])
     sql = "DELETE FROM book_classes WHERE book_id = ?"
     db.execute(sql, [book_id])
     sql = "DELETE FROM books WHERE id = ?"
